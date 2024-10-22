442c327361b6aacf0530b2bb9ddd05d659b1d535
dumalbhaskar <dumalbhaskar@gmail.com>
2024-10-23 00:47:02 +0530

    tf-eks

--- Task09/modules/ec2/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.app_server.id
-}
-
-output "security_group_name" {
-  description = "ID of the EC2 instance"
-  value       = aws_security_group.test.name
-}
--- Task10/modules/s3/main.tf
+++
@@ -1,7 +0,0 @@
-
-resource "aws_s3_bucket" "mytestbucket" {
-  count = var.create_bucket ? 1 : 0
-
-  bucket = var.bucket_name
-  
-}
